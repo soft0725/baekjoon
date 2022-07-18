828e399aa9d91f41a99db101c8616f5b86c0c3b5
chan <a01021146076@gmail.com>
2022-07-18 12:21:58 +0900

    baekjoon

---
+++ 백준/2522번(별 찍기 - 12)/별 찍기 - 12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int n;
+	scanf("%d",&n);
+	
+	for(int i = 0; i < n; i++){
+		for(int j = n - 1; j > i; j--){
+			printf(" ");
+		}
+		for(int k = 0; k <= i; k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	for(int i = 0; i < n - 1; i++){
+		for(int j = 0; j <= i; j++){
+			printf(" ");
+		}
+		for(int k = n - 1; k > i; k--){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
